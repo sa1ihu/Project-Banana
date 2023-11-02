9975d8803491a08f959c8fe125b2dd79ca4289bf
salihusalihusalihu <143562129+salihusalihusalihu@users.noreply.github.com>
2023-11-02 21:25:38 +0100

    Add files via upload

---
+++ final.py
@@ -0,0 +1,20 @@
+
+def classify(sequence):
+    sequence = sequence.upper()
+    valid_dna = set("ATGC")
+    valid_rna = set("AUGC")
+    if set(sequence) <= valid_dna:
+        return "DNA"
+    elif set(sequence) <= valid_rna:
+        return "RNA"
+    else:
+        return "Invalid sequence"
+
+while True:
+    sequence = str(input("Enter the sequence: "))
+    result = classify(sequence)
+    if result != "Invalid sequence":
+        print(f"The sequence is classified as {result}")
+    else:
+        print("Error: Invalid sequence")
+
